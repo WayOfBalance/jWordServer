3bc717991de4b992ce272d00987516cb41322d89
Roman.Pro <136555465+RomanPronskiy@users.noreply.github.com>
2024-01-06 19:22:55 +0200

    test 2

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,6 +6,10 @@ func routes(_ app: Application) throws {
     }
 
     app.get("hello") { req async -> String in
-        "Олечка привет я создал и запустил нам наш первый сервер"
+        "Миша привет я создал и запустил нам наш первый сервер"
+    }
+
+    app.get("new") { req async -> String in
+        "Миша, привет, работа идет, все в порядке, полет нормальный"
     }
 }
